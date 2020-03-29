7fa108994e466526876821d87b8c1cfc06ad0f25
Christian Hammer <christian.hammer@dynarize.de>
2020-03-29 15:46:04 +0200

    using subtests

--- scramble_test.go
+++ scramble_test.go
@@ -1,21 +1,24 @@
 package scramble
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestScramble(t *testing.T) {
-	testData := map[string]string {
-		"Hello World": "WeHodo lllr",
-		"john.doe@company.test": "opon.ocdtehsa.t@mjeyn",
-	}
-	for given, expected := range testData {
-		fmt.Printf("%s -> %s\n", given, expected)
+var scrambleTestData = []struct {
+	given  string
+	expected string
+}{
+	{"Hello World", "WeHodo lllr"},
+	{"john.doe@company.test", "opon.ocdtehsa.t@mjeyn"},
+}
 
-		got := Scramble(given)
-		if got != expected {
-			t.Errorf("Scramble('%s') = %s, expected %s", given, got, expected)
-		}
+func TestScramble(t *testing.T) {
+	for _, testData := range scrambleTestData {
+		t.Run(testData.given, func(t *testing.T) {
+			got := Scramble(testData.given)
+			if got != testData.expected {
+				t.Errorf("Scramble('%s') = %s, expected %s", testData.given, got, testData.expected)
+			}
+		})
 	}
 }
