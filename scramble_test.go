7dc230c00021a51505e3789bd4853c677fb8909c
Christian Hammer <christian.hammer@dynarize.de>
2020-03-29 15:22:01 +0200

    cleaned up test

--- scramble_test.go
+++ scramble_test.go
@@ -1,19 +1,21 @@
 package scramble
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestScramble(t *testing.T) {
-	got := Scramble("Hello World")
-	if got != "WeHodo lllr" {
-		t.Errorf("Hello() = %s, want %s", got, "x")
+	testData := map[string]string {
+		"Hello World": "WeHodo lllr",
+		"john.doe@company.test": "opon.ocdtehsa.t@mjeyn",
 	}
-}
+	for given, expected := range testData {
+		fmt.Printf("%s -> %s\n", given, expected)
 
-func TestScramble2(t *testing.T) {
-	got := Scramble("john.doe@company.test")
-	if got != "opon.ocdtehsa.t@mjeyn" {
-		t.Errorf("Hello() = %s, want %s", got, "x")
+		got := Scramble(given)
+		if got != expected {
+			t.Errorf("Scramble('%s') = %s, expected %s", given, got, expected)
+		}
 	}
 }
