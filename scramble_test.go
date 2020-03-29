dff49e7f9ff2ed25ddb22a54968348239f12a9d0
Christian Hammer <christian.hammer@dynarize.de>
2020-03-29 15:55:13 +0200

    running tests in parallel .. ha ha, why not ..

--- scramble_test.go
+++ scramble_test.go
@@ -10,13 +10,17 @@ var scrambleTestData = []struct {
 }{
 	{"Hello World", "WeHodo lllr"},
 	{"john.doe@company.test", "opon.ocdtehsa.t@mjeyn"},
+	{"你好，世界", "你，好世界"},
 }
 
 func TestScramble(t *testing.T) {
+	t.Parallel()
 	for _, testData := range scrambleTestData {
+		testData := testData
 		t.Run(testData.given, func(t *testing.T) {
 			got := Scramble(testData.given)
 			if got != testData.expected {
+				t.Parallel()
 				t.Errorf("Scramble('%s') = %s, expected %s", testData.given, got, testData.expected)
 			}
 		})
